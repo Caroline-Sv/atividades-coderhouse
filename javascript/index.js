beb0f911e1b7032de0cbb29579c33834fc9062bd
Caroline da Silva <caroline.svs@hotmail.com>
2024-02-14 23:00:45 -0300

    aplicando recursos de javascript

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+let nome = 'Carol'
+let sobrenome = 'Silva'
+let anoAtual = 2024
+let idadeFinal = 26
+
+const nomeCompleto = `${nome} ${sobrenome}`
+const anoNascimento = anoAtual - idadeFinal
+
+console.log(nomeCompleto)
+console.log(anoNascimento)
+
+
+
+let numero1 = prompt('insira o numero 1 aqui')
+let numero2 = prompt('insira o numero 2 aqui')
+
+console.log(parseInt(numero1) + parseInt(numero2))
+
+alert('O valor é:' + parseInt(numero1) * parseInt(numero2))
